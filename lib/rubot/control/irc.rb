baef179573aa3d8041c415bed0c7eb973a459cb4
chrislee35 <rubygems@chrislee.dhs.org>
2015-10-24 22:14:10 +0900

    added logger and moved monkeypatch of EventMachine::IRC::Client out of the testing into this files

--- lib/rubot/control/irc.rb
+++ lib/rubot/control/irc.rb
@@ -1,3 +1,21 @@
 require 'em-irc'
+require 'logger'
 
+# monkey patching broken gem
+module EventMachine
+  module IRC
+    class Client
+      def unbind(reason)
+        log Logger::INFO "Unbind reason: #{reason}" if reason != nil
+        trigger(:disconnect)
+      end
+    end
+  end
+end
+
+
+module Rubot
+  module Control
+  end
+end
 Rubot::Control::IRC = EventMachine::IRC::Client
